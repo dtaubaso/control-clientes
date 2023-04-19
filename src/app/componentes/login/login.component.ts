a0f1898582bdd1972be72e9eb7f7c9f13af040ba
dtaubaso <d.taubaso@gmail.com>
2023-04-18 23:18:40 -0300

    componente de login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  email: string;
+  password: string;
+
+  constructor(
+    private router: Router, 
+    private flashMessages: FlashMessagesService,
+    private loginservice: LoginService
+  ){}
+
+    login(){
+      this.loginservice.login(this.email, this.password)
+        .then( res => {
+          this.router.navigate(["/"]);
+        })
+        .catch(error=>{
+          this.flashMessages.show(error.message, {
+            cssClass: 'alert-danger', timeout:4000
+          });
+        });
+    }
 
 }
