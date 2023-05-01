3c6d4822e37793fdd3689996cfd539d764896943
dtaubaso <d.taubaso@gmail.com>
2023-05-01 19:02:04 -0300

    componente de registro

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  email: string;
+  password: string;
+
+  constructor(
+    private router: Router, 
+    private flashMessages: FlashMessagesService,
+    private loginservice: LoginService
+  ){}
+
+  ngOnInit(){
+    this.loginservice.getAuth().subscribe( auth =>{
+      if(auth){
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  registro(){
+    this.loginservice.registrarse(this.email, this.password)
+    .then( res => {
+      this.router.navigate(["/"]);
+    })
+    .catch(error => {
+      this.flashMessages.show(error.message, {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    });
+  }
 
 }
