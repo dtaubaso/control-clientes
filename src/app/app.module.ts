4509f12f91be6062ab863ba9765b225fec78663b
dtaubaso <d.taubaso@gmail.com>
2023-05-01 22:06:05 -0300

    guardian de configuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ClienteServicio } from './servicios/cliente.service';
 import { LoginService } from './servicios/login.service';
 import { AuthGuard } from './guardianes/auth.guard';
 import { ConfiguracionServicio } from './servicios/configuracion.service';
+import { ConfiguracionGuard } from './guardianes/configuracion.guard';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,9 @@ import { ConfiguracionServicio } from './servicios/configuracion.service';
   providers: [ClienteServicio,
     LoginService,
     AuthGuard,
-    ConfiguracionServicio],
+    ConfiguracionServicio, 
+    ConfiguracionGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
